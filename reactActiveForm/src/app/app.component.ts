7d9e91f8760e3e869ba9d115af908224b3fa47dc
ahmedmili <ahmedmili76@gmail.com>
2022-08-04 12:21:45 +0100

    reactiveForm'

---
+++ reactActiveForm/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+   mailListe: Array<any> = [];
+
+  // title = 'reactActiveForm';
+
+  form = new FormGroup({
+    first_name: new FormControl('',[Validators.required,Validators.maxLength(6)]),
+    last_name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required])
+    })
+
+    onSubmit(){
+      this.mailListe.push(this.form.value.email)
+      console.log(this.form.value.email)
+    }
+
+}
